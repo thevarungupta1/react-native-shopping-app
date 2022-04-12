84c48593d1c7be5934cc1bd81e29745cbc8a105a
Varun Gupta <thevarungupta5@gmail.com>
2022-04-12 16:04:41 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import ProductsList from "./screens/ProductList";
+import ProductDetails from "./screens/ProductDetails";
+import { CartProvider } from "./CartContext";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Cart } from "./screens/Cart";
+import { CartIcon } from "./components/CartIcon";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <CartProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Products" component={ProductsList} options={({navigation}) => ({title: 'Products', headerRight: () => <CartIcon navigation={navigation} />})} />
+          <Stack.Screen name="ProductDetails" component={ProductDetails} />
+          <Stack.Screen name="Cart" component={Cart} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CartProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  Container: {
+    textAlign: 'center',
     alignItems: 'center',
     justifyContent: 'center',
+    flex: 1
   },
-});
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+})
---
+++ screens/ProductList.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { getProducts } from "../services/ProductsService";
+import Product from "../components/Product";
+
+
+export default function ProductsList({navigation}) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(getProducts());
+  });
+
+  function renderProduct({ item: product }) {
+    return <Product {...product} onPress={() => {
+      navigation.navigate('ProductDetails', { productId: product.id })
+    }} />;
+  }
+
+  return (
+    <FlatList
+      style={styles.productsList}
+      contentContainerStyle={styles.productsListContainer}
+      keyExtractor={(item) => item.id.toString()}
+      data={products}
+      renderItem={renderProduct}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  productsList: {
+    backgroundColor: "#eeeeee",
+  },
+  productsListContainer: {
+    backgroundColor: "#eeeeee",
+    paddingVertical: 8,
+    marginHorizontal: 8,
+  },
+});
